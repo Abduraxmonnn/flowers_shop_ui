5ed9d0d7742286160bcbbaef866f3f4f75ae4e34
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2024-01-19 21:34:40 +0500

    header done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+
+import { images } from '../../constants'; 
 import './Header.css'
 
 const Header = () => (
-	<div>
-		<h1>Header</h1>
-	</div>
+	<nav className='app__header'>
+		<div className='app__header-logo'>
+			<img src={images.logo} alt='logo' />
+		</div>
+
+		<ul className='app__header-links'>
+			<li className='p_quicksand'>Home</li>
+			<li className='p_quicksand'>About Us</li>
+			<li className='p_quicksand'>Services</li>
+			<li className='p_quicksand'>Shop</li>
+			<li className='p_quicksand'>Contact Us</li>
+		</ul>
+
+		<div className='app__header_lang-switcher'>
+			<p className='app__header-switcher'>Eng</p>
+			<img src={images.arrow_down} alt='arrow' />
+		</div>
+	</nav>
 )
 export default Header
---
+++ src/constants/images.js
@@ -0,0 +1,8 @@
+import logo from '../assets/logo.png';
+import arrow_down from '../assets/arrow-down.png';
+
+
+export default {
+    logo,
+    arrow_down
+}
