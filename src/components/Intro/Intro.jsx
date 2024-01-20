47d8c8bd0406aa7a6f27cae7a53de2c4d85ea202
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2024-01-20 14:53:40 +0500

    Started INtro

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+
+import { images } from '../../constants';
 import './Intro.css'
 
-const Intro = () => (
-	<div>
-		<h1>Intro</h1>
-	</div>
-)
+const Intro = () => {
+	return (
+		<div className='app__intro flex__center'>
+			{/* <div className='app__intro-overlay flex__center'>
+				<img src={images.intro_background} alt='background' />
+			</div> */}
+
+			<div className='app__intro-content'>
+				<div className='app__intro-title'>
+					<p>Fresh Flower & Gift Shop</p>
+					<h1>Making beautiful flowers a part of your life.</h1>
+				</div>
+
+				<div className='app__intro-button'>
+					<button className='app__intro-button_shop-now'>
+						Shop Now
+					</button>
+				</div>
+			</div>
+		</div>
+	)
+}
 export default Intro
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,8 +1,10 @@
 import logo from '../assets/logo.png';
 import arrow_down from '../assets/arrow-down.png';
+import intro_background from '../assets/intro-background.png';
 
 
 export default {
     logo,
-    arrow_down
+    arrow_down,
+    intro_background
 }
